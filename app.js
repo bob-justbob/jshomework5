fc63eb2490cf66901adf1d9ed451be822c5fb425
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-12-07 15:58:55 +0400

    adding exit

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ const form = document.querySelector('.form')
 const ul = document.querySelector('ul')
 const input = document.querySelector('input[name="thing"]')
 const logbutton = document.querySelector('.logbutton')
-
-
+const loggbutton = document.querySelector('.loggbutton')
+const errorMsg = document.getElementById('errorMSg')
+const exit = document.querySelector('.exit')
  
+
 logbutton.addEventListener("click", function(event){
     event.preventDefault();
     const popup = document.querySelector('.popup')
     popup.style.display = "flex"
+    
+    exit.onclick = function(){
+        popup.remove();
+        return;
+    }
+});
+
+
+
+loggbutton.addEventListener('click', function(event){
+    event.preventDefault();
+    errorMsg.value = "";
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username == "" || password == "") {
+        
+        errorMsg.textContent = "user or passord are incorect try again"
+    } 
+
 })
+
 
 
 
@@ -31,6 +54,7 @@ form.addEventListener('submit', (event) =>{
         span.textContent = input.value;
 
         const li = document.createElement('li')
+        li.style.textDecoration = "none"
 
         li.appendChild(button)
         li.appendChild(span)
